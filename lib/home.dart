e40eab0c84b4554f0641db21ff710ee1e13fec21
mdzarepour <mdzarepour@gmail.com>
2025-07-03 22:30:39 +0330

    dependencies added

---
+++ lib/home.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center());
+  }
+}
