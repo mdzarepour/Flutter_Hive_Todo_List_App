e3acf23cecfcb44326321429f7bba5f0031bb230
mdzarepour <mdzarepour@gmail.com>
2025-07-18 12:37:25 +0330

    fix: hive adapter generating error fixed by rebuilding adapter

--- lib/data/hive_data.dart
+++ lib/data/hive_data.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/foundation.dart';
-import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:hive_todo/models/task_model.dart';
 
@@ -7,26 +6,21 @@ class HiveData {
   static const boxName = 'taskBox';
   final Box<TaskModel> taskBox = Hive.box(boxName);
 
-  // create new task -->
   Future<void> addNewTask({required String id, required TaskModel task}) async {
     await taskBox.put(id, task);
   }
 
-  // read task -->
   Future<TaskModel?> readTask({required String id}) async {
     return taskBox.get(id);
   }
 
-  // update task -->
   Future<void> updateTask({required TaskModel task}) async {
     await task.save();
   }
 
-  // delete task -->
   Future<void> deleteTask({required TaskModel task}) async {
     await task.delete();
   }
 
-  // listen to box changes -->
   ValueListenable<Box<TaskModel>> listenToBox() => taskBox.listenable();
 }
--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,32 @@ import 'package:hive_todo/data/hive_data.dart';
 import 'package:hive_todo/models/task_model.dart';
 import 'package:hive_todo/views/home/home_view.dart';
 
-void main(List<String> args) async {
+void main() async {
   await Hive.initFlutter();
-  Hive.registerAdapter<TaskModel>(TaskModelAdapter());
-  var box = Hive.openBox<TaskModel>(HiveData.boxName);
-  runApp(const Application());
+  Hive.registerAdapter(TaskModelAdapter());
+  await Hive.openBox<TaskModel>('taskBox');
+  runApp(BaseWidget(child: const Application()));
+}
+
+class BaseWidget extends InheritedWidget {
+  final HiveData hiveData = HiveData();
+
+  final Widget child;
+  BaseWidget({super.key, required this.child}) : super(child: child);
+
+  static BaseWidget of(BuildContext context) {
+    final base = context.dependOnInheritedWidgetOfExactType<BaseWidget>();
+    if (base != null) {
+      return base;
+    } else {
+      throw StateError('could not found encestor widget of type BaseWidget');
+    }
+  }
+
+  @override
+  bool updateShouldNotify(covariant InheritedWidget oldWidget) {
+    return false;
+  }
 }
 
 class Application extends StatelessWidget {
